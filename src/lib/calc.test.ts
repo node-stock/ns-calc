6ee973e2ace47c1ab7b579df31953e490741b186
zlq4863947 <zlq4863947@gmail.com>
2017-12-31 10:44:19 +0900

    代码提交

--- src/lib/calc.test.ts
+++ src/lib/calc.test.ts
@@ -1,11 +1,110 @@
 import { Calc } from './calc';
+import * as types from 'ns-types';
 import 'jest';
 
-const test = async () => {
+const testOpenPosition = () => {
+
+  const account: types.Account = {
+    id: 'test',
+    balance: '1230004.12442',
+    bitcoin: '0.025',
+    backtest: '1',
+    positions: [],
+    transactions: []
+  }
+  const order: types.LimitOrder = {
+    symbol: types.Pair.BTC_JPY,
+    price: '1675000',
+    amount: '0.00421125',
+    symbolType: types.SymbolType.cryptocoin,
+    eventType: types.EventType.Order,
+    tradeType: types.TradeType.Spot,
+    orderType: types.OrderType.Limit,
+    side: types.OrderSide.Buy,
+    backtest: '1'
+  }
+  const res = Calc.openPosition({
+    account, order
+  });
+  console.log(res);
+}
+
+const testOpenPosition2 = () => {
+  const positions: types.Position[] = [{
+    account_id: 'test',
+    symbol: types.Pair.BTC_JPY,
+    type: types.SymbolType.cryptocoin,
+    side: types.OrderSide.Buy,
+    price: '1804897',
+    quantity: '0.00325',
+    backtest: '1'
+  }];
+
+  const account: types.Account = {
+    id: 'test',
+    balance: '1230004.12442',
+    bitcoin: '0.025',
+    backtest: '1',
+    positions,
+    transactions: []
+  }
+  const order: types.LimitOrder = {
+    symbol: types.Pair.BTC_JPY,
+    price: '1675000',
+    amount: '0.00421125',
+    symbolType: types.SymbolType.cryptocoin,
+    eventType: types.EventType.Order,
+    tradeType: types.TradeType.Spot,
+    orderType: types.OrderType.Limit,
+    side: types.OrderSide.Buy,
+    backtest: '1'
+  }
+  const res = Calc.openPosition({
+    account, order
+  });
+  console.log(res);
+}
+
+const testClosePosition = () => {
+  const positions: types.Position[] = [{
+    account_id: 'test',
+    symbol: types.Pair.BTC_JPY,
+    type: types.SymbolType.cryptocoin,
+    side: types.OrderSide.Buy,
+    price: '1804897',
+    quantity: '0.00325',
+    backtest: '1'
+  }];
+
+  const account: types.Account = {
+    id: 'test',
+    balance: '1230004.12442',
+    bitcoin: '0.025',
+    backtest: '1',
+    positions,
+    transactions: []
+  }
+  const order: types.LimitOrder = {
+    symbol: types.Pair.BTC_JPY,
+    price: '1675000',
+    amount: '0.00321125',
+    symbolType: types.SymbolType.cryptocoin,
+    eventType: types.EventType.Order,
+    tradeType: types.TradeType.Spot,
+    orderType: types.OrderType.Limit,
+    side: types.OrderSide.BuyClose,
+    backtest: '1'
+  }
+  const res = Calc.closePosition({
+    account, order
+  });
+  console.log(res);
 }
 
 describe('ns-calc', () => {
 
-  it('测试更新订单状态', test);
+  // it('测试多单建仓', testOpenPosition);
+  // it('测试多单建仓2', testOpenPosition2);
+  it('测试多单平仓', testClosePosition);
 
 });
--- src/lib/calc.ts
+++ src/lib/calc.ts
@@ -1,5 +1,6 @@
 import * as types from 'ns-types';
 import { BigNumber } from 'BigNumber.js';
+import { Log, Util } from 'ns-common';
 
 /**
   * @class
@@ -7,26 +8,41 @@ import { BigNumber } from 'BigNumber.js';
   */
 export class Calc {
 
-  static closePosition(options: {
+  static openPosition(options: {
     account: types.Account,
-    order: types.Order,
-    position?: types.Position
+    order: types.Order
   }) {
-    const posiQty = options.position ? new BigNumber(options.position.quantity) : undefined;
+    Log.system.info('计算建仓[启动]');
+    Log.system.info(`账户信息：${JSON.stringify(options.account, null, 2)}，
+    订单：${JSON.stringify(options.order, null, 2)}`);
+    if (options.order.side !== types.OrderSide.Buy
+      && options.order.side !== types.OrderSide.Sell) {
+      Log.system.error(`订单方向${options.order.side}异常!`);
+      return;
+    }
+    if (options.order.side === types.OrderSide.Sell) {
+      Log.system.error(`暂时未对应做空!`);
+      return;
+    }
 
     switch (options.order.side) {
       // 多单买入
       case types.OrderSide.Buy:
+        Log.system.info('多单建仓');
+        let position = options.account.positions.find(o =>
+          o.symbol === options.order.symbol && o.side === types.OrderSide.Buy
+        );
         // 已有持仓
-        if (options.position) {
+        if (position) {
+          Log.system.info('已有持仓信息：', JSON.stringify(position, null, 2));
           // 更新数量（已有数量+订单数量）
-          options.position.quantity = new BigNumber(options.position.quantity)
-            .plus(new BigNumber(options.order.amount)).toFormat(8);
+          position.quantity = new BigNumber(position.quantity)
+            .plus(new BigNumber(options.order.amount)).toString();
           // 更新持仓均价（(已有仓位价格+订单价格）/2）
-          options.position.price = new BigNumber(options.position.price)
-            .plus(options.order.price).div(2).toFormat(8);
+          position.price = new BigNumber(position.price)
+            .plus(options.order.price).div(2).toString();
         } else { // 建仓
-          options.position = {
+          position = {
             account_id: options.account.id,
             symbol: options.order.symbol,
             type: options.order.symbolType,
@@ -36,30 +52,100 @@ export class Calc {
             backtest: options.order.backtest,
             mocktime: options.order.mocktime
           }
+          Log.system.info('查询到持仓信息，建仓：', JSON.stringify(position, null, 2));
+          options.account.positions.push(position);
         }
         // 更新余额（余币）
-        // 购买总价 = 订单股价*订单股数
-        const buyTotal = new BigNumber(options.order.price).
+        // 购买总价 = 订单价格*订单数量
+        const contractPrice = new BigNumber(options.order.price).
           times(new BigNumber(options.order.amount));
-        // TODO
-        const useBitcoin = false;
-        const fee = 0;
-        if (useBitcoin) {
-          // 余币 = 当前余币 - 股数
-          options.account.bitcoin = new BigNumber(options.account.bitcoin)
-            .minus(options.order.amount).minus(fee).toFormat(8);
-        } else {
-          // 余额 = 当前余额 - 购买总价 - 手续费
-          options.account.balance = new BigNumber(options.account.balance)
-            .minus(buyTotal).minus(fee).toFormat(8);
-        }
-        return {
-          account: options.account,
-          position: options.position
-        };
+        Log.system.info(`购买总价(${contractPrice.toString()}) =
+          订单价格(${options.order.price}) * 订单数量(${options.order.amount})`);
 
-      default:
+        const useBitcoin = Util.getTradeUnit(options.order.symbol).type === 'btc';
+        const fee = Util.getFee(options.order.symbol);
+        if (useBitcoin) {
+          const lastBitcoin = options.account.bitcoin;
+          // 余币 = 当前余币 - 购买总价 - 手续费
+          options.account.bitcoin = new BigNumber(lastBitcoin)
+            .minus(contractPrice).minus(fee).toString();
+          Log.system.info(`余币(${options.account.bitcoin}) =
+            当前余币(${lastBitcoin}) - 购买总价(${contractPrice}) - 手续费(${fee})`);
+        } else {
+          const lastBalance = options.account.balance;
+          // 余额 = 当前余额 - 购买总价 - 手续费
+          options.account.balance = new BigNumber(lastBalance)
+            .minus(contractPrice).minus(fee).toString();
+          Log.system.info(`余额(${options.account.balance}) =
+              当前余额(${lastBalance}) - 购买总价(${contractPrice}) - 手续费(${fee})`);
+        }
         break;
     }
+    Log.system.info('计算建仓[终了]');
+    return Object.assign(options.account);
+  }
+
+  static closePosition(options: {
+    account: types.Account,
+    order: types.Order
+  }) {
+    Log.system.info('计算平仓[启动]');
+    Log.system.info(`账户信息：${JSON.stringify(options.account, null, 2)}，
+    订单：${JSON.stringify(options.order, null, 2)}`);
+    if (options.order.side !== types.OrderSide.BuyClose
+      && options.order.side !== types.OrderSide.SellClose) {
+      Log.system.error(`订单方向${options.order.side}异常!`);
+      return;
+    }
+    if (options.order.side === types.OrderSide.SellClose) {
+      Log.system.error(`暂时未对应空单平仓!`);
+      return;
+    }
+
+    switch (options.order.side) {
+      // 多单平仓
+      case types.OrderSide.BuyClose:
+        Log.system.info('多单平仓');
+        const position = options.account.positions.find(o =>
+          o.symbol === options.order.symbol && o.side === types.OrderSide.Buy
+        );
+        // 已有持仓
+        if (position) {
+          Log.system.info('已有持仓信息：', JSON.stringify(position, null, 2));
+          // 更新数量（已有数量-订单数量）
+          position.quantity = new BigNumber(position.quantity)
+            .minus(new BigNumber(options.order.amount)).toString();
+        } else {
+          Log.system.error('平多出错，未找到多单持仓[异常终了]');
+          return;
+        }
+        // 更新余额（余币）
+        // 平仓总价 = 订单价格*订单数量
+        const contractPrice = new BigNumber(options.order.price).
+          times(new BigNumber(options.order.amount));
+        Log.system.info(`平仓总价(${contractPrice.toString()}) =
+          订单价格(${options.order.price}) * 订单数量(${options.order.amount})`);
+
+        const useBitcoin = Util.getTradeUnit(options.order.symbol).type === 'btc';
+        const fee = Util.getFee(options.order.symbol);
+        if (useBitcoin) {
+          const lastBitcoin = options.account.bitcoin;
+          // 余币 = 当前余币 + 平仓总价 - 手续费
+          options.account.bitcoin = new BigNumber(lastBitcoin)
+            .plus(contractPrice).minus(fee).toString();
+          Log.system.info(`余币(${options.account.bitcoin}) =
+            当前余币(${lastBitcoin}) - 平仓总价(${contractPrice}) - 手续费(${fee})`);
+        } else {
+          const lastBalance = options.account.balance;
+          // 余额 = 当前余额 + 购买总价 - 手续费
+          options.account.balance = new BigNumber(lastBalance)
+            .plus(contractPrice).minus(fee).toString();
+          Log.system.info(`余额(${options.account.balance}) =
+              当前余额(${lastBalance}) + 购买总价(${contractPrice}) - 手续费(${fee})`);
+        }
+        break;
+    }
+    Log.system.info('计算平仓[终了]');
+    return Object.assign(options.account);
   }
 }
